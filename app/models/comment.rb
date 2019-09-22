4e26b42e0c20052a89865a3d0bb1893d51a4a5a8
nenekelvin <karvinmitchar@gmail.com>
2019-09-22 14:49:01 +0300

    initialized pusher client triggers

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+      after_commit :notify_pusher, on: [:create, :update]
+      def notify_pusher
+      Pusher.trigger('comment', 'new', self.as_json)
+      end
 end
